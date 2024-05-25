acaf3e2dde98f3a9898b70487d45a5144f49fe4c
Dmitriy Kurilko <dimakurilko0606@mail.ru>
2024-05-25 05:40:34 +0300

    fix get_current_exchange_rate

--- src/utils.py
+++ src/utils.py
@@ -29,8 +29,25 @@ def get_current_exchange_rate(transaction: dict[str, Any]) -> float:
         url = f"https://api.apilayer.com/exchangerates_data/latest?symbols=RUB&base={currency}"
         headers = {"apikey": API_KEY}
         response = requests.get(url, headers=headers).json()
-        return float(response["rates"]["RUB"] * transaction["operationAmount"]["amount"])
+        return response["rates"]["RUB"] * float(transaction["operationAmount"]["amount"])
 
     except (requests.exceptions.RequestException, KeyError, ValueError) as e:
         print(f"An error {e}")
         return 1.0
+
+
+print(get_current_exchange_rate({
+    "id": 619287771,
+    "state": "EXECUTED",
+    "date": "2019-08-19T16:30:41.967497",
+    "operationAmount": {
+        "amount": "81150.87",
+        "currency": {
+            "name": "USD",
+            "code": "USD"
+        }
+    },
+    "description": "Перевод организации",
+    "from": "Счет 17691325653939384901",
+    "to": "Счет 49304996510329747621"
+}))
